a2bd5be462f83de3b8e7504b98b18a4f50164f82
GentaMorino <genta.morino@gmail.com>
2025-02-09 06:40:59 +0900

    ファストビュー作成

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
     name: 'ブログ投稿',
-    description: '',
+    description: 'ブログ投稿ができるWebアプリケーションです。',
     url: 'http://localhost:3000',
     ogImage: '',
     links: {
